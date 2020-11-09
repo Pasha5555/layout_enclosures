7c18b42d1b681472462d3b934783440f8dcda5d6
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2020-11-10 01:10:57 +0200

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,41 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20201110-004907\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/enclosures/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Test file not found C:\\Users\\User\\Desktop\\PROJECTS\\layout_enclosures\\backstop_data\\bitmaps_test\\20201110-004907\\Entire_document.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Single_block_with_id_qa-block.png",
+        "test": "..\\bitmaps_test\\20201110-004907\\Single_block_with_id_qa-block.png",
+        "selector": "#qa-block",
+        "fileName": "Single_block_with_id_qa-block.png",
+        "label": "Single block with id_qa-block\"",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/enclosures/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Test file not found C:\\Users\\User\\Desktop\\PROJECTS\\layout_enclosures\\backstop_data\\bitmaps_test\\20201110-004907\\Single_block_with_id_qa-block.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
